93f886a535f23def477ec6b69320fc6dc1d5afe6
IngeQuiroga <ingequiroga1@hotmail.com>
2022-07-25 22:35:22 -0500

    se agregan inserts

---
+++ InmueblesDatos/02_Configuracion/04_INSERT_tb_recuperadorabanco.sql
@@ -0,0 +1,8 @@
+SELECT * FROM inmuebles.tb_recuperadorabanco;
+
+INSERT INTO `tb_recuperadorabanco`(`Tipo`,`Nombre`) VALUES (1,'HSBC');
+INSERT INTO `tb_recuperadorabanco`(`Tipo`,`Nombre`) VALUES (1,'Santander');
+INSERT INTO `tb_recuperadorabanco`(`Tipo`,`Nombre`) VALUES (1,'Bancomer');
+INSERT INTO `tb_recuperadorabanco`(`Tipo`,`Nombre`) VALUES (1,'Banamex');
+INSERT INTO `tb_recuperadorabanco`(`Tipo`,`Nombre`) VALUES (2,'Recuperadora 1');
+INSERT INTO `tb_recuperadorabanco`(`Tipo`,`Nombre`) VALUES (2,'Recuperadora 2');
---
+++ InmueblesDatos/02_Configuracion/05_INSERT_tb_tab_subetapas.sql
@@ -0,0 +1,10 @@
+select * from tb_tab_subetapas;
+
+INSERT INTO tb_tab_subetapas (`Descripcion`) VALUES ('Propuesta de Compra');
+
+INSERT INTO tb_tab_subetapas (`Descripcion`) VALUES ('Referencia de Pago');
+INSERT INTO tb_tab_subetapas (`Descripcion`) VALUES ('Expediente');
+INSERT INTO tb_tab_subetapas (`Descripcion`) VALUES ('Pago');
+INSERT INTO tb_tab_subetapas (`Descripcion`) VALUES ('Firma de cesión');
+INSERT INTO tb_tab_subetapas (`Descripcion`) VALUES ('Recuperacion extra Judicial');
+INSERT INTO tb_tab_subetapas (`Descripcion`) VALUES ('Contrato privado');
