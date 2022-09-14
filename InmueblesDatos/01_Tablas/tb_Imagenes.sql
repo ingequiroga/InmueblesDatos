765eee87f1e6d1c929f9750a846532e4824003d3
IngeQuiroga <ingequiroga1@hotmail.com>
2022-09-13 19:35:37 -0500

    SE agrega campo a tabla de imagenes

--- InmueblesDatos/01_Tablas/tb_Imagenes.sql
+++ InmueblesDatos/01_Tablas/tb_Imagenes.sql
@@ -3,8 +3,9 @@ DROP TABLE IF EXISTS `tb_Imagenes`;
  CREATE TABLE `inmuebles`.`tb_Imagenes` (
   `IdImagen` INT NOT NULL AUTO_INCREMENT,
   `Image` longblob DEFAULT '' NOT NULL ,
+  `FileName` Varchar(100) DEFAULT '' NOT NULL ,
   `FechaCreacion` DATE DEFAULT (CURRENT_DATE) NOT NULL,
   `IdAdquisicion` INT DEFAULT 0 NOT NULL ,
-  `IdEtapa` INT DEFAULT 0 NOT NULL ,
+  `IdProceso` INT DEFAULT 0 NOT NULL ,
   `IdSubEtapa` INT DEFAULT 0 NOT NULL ,
   PRIMARY KEY (`IdImagen`));
